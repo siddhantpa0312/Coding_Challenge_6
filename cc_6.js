bddda06be383f5b5ebbc2f7f96351018d3d1056c
Siddhant Pandey <siddhant.pa2002@gmail.com>
2026-03-01 21:47:00 -0500

    Created Subclass

--- cc_6.js
+++ cc_6.js
@@ -7,3 +7,14 @@ class Employee {
         return `${this.name} works in ${this.department} department.`;
     }
 }
+
+class Manager extends Employee {
+    constructor(name, department, teamSize){
+        super(name, department);
+        this.teamSize = teamSize;
+    }
+    describe(){
+        return `${this.name} is a manager in the ${this.department} and manages a team of ${this.teamSize} employees.`
+    }
+
+}
