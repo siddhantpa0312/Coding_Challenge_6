d65ebd79657304128cd54fc4c57c1860a05c448d
Siddhant Pandey <siddhant.pa2002@gmail.com>
2026-03-01 22:17:59 -0500

    Created Sample Employees, Created Company Class, Instantiated Company object

--- cc_6.js
+++ cc_6.js
@@ -18,3 +18,34 @@ class Manager extends Employee {
     }
 
 }
+
+const emp1 = new Employee ("Siddhant Pandey", "Finance");
+const emp2 = new Employee ("Brandon Dye", "Sales");
+const emp3 = new Employee ("Aron Buttles", "Human Resources");
+
+const mngr1 = new Manager ("John Smith", "IT", 13);
+const mngr2 = new Manager ("Emilia Clarkson", "Finance", 25);
+
+class Company {
+    constructor() {
+        this.employees = [];
+    }
+    addEmployee (Employee){
+        this.employees.push(Employee);
+    }
+    listEmployees(){
+        this.employees.forEach(Employee => {
+            console.log(Employee.describe());
+        });
+    }
+}
+
+const Company = new Company();
+
+Company.addEmployee(emp1);
+Company.addEmployee(emp2);
+Company.addEmployee(mngr1);
+Company.addEmployee(mngr2);
+
+Company.listEmployees();
+
